615aa36877787cd6aa41c5cd563959c4deb03caa
lime-rina <marinayordanova@limechain.tech>
2024-05-09 16:50:09 +0300

    Setting Up A First Route & Handling a First Request

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	gin.Default()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") //lh:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "hello"})
 }
