168e83ad7a7ea01aea656464e459a0186de6df70
lime-rina <marinayordanova@limechain.tech>
2024-05-10 11:41:38 +0300

    added a db setup and queries for getting events and creating

--- main.go
+++ main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"net/http"
 
+	"example.com/main/db"
+	"example.com/main/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //lh:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "hello"})
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't get events!"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't create event!"})
+		context.Error(err)
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err1 := event.Save()
+	if err1 != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't create event!"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
