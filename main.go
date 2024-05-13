7c5cea3ad9af19b2c6024cd43c12f81c607c9717
lime-rina <marinayordanova@limechain.tech>
2024-05-13 09:42:29 +0300

    Added get event by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/main/db"
 	"example.com/main/models"
@@ -13,6 +14,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") //lh:8080
@@ -25,6 +27,22 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldn't get event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"event": event})
 }
 
 func createEvent(context *gin.Context) {
